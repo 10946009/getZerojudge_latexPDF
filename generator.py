0868af3c255feca7cf2fce8e07f48094444003ee
10946009 <108606810+10946009@users.noreply.github.com>
2023-02-06 19:40:56 +0800

    add generate_random的用法，修改secret的測資檔名
    generate_random_one(最小值,最大值)，會回傳一個亂數且自動塞入inputlist
    generate_random_many(亂數數量,最小值,最大值,是否可重複,是否排序)，會回傳一個亂數list且自動塞入inputlist
    generate_random_first_many(第一個數字最小值,第一個數字最大值,亂數list的最小值,亂數list的最大值,是否可重複,是否排序)，根據第一個的數字決定後面要產多少數字，會回傳一個亂數list且自動塞入inputlist

--- generator.py
+++ generator.py
@@ -3,6 +3,45 @@ import subprocess
 import os
 import random
 import glob
+############################################################################
+def generate_random_one(m_min,m_max):
+    global inputlist
+    m_max += 1
+    r = random.randrange(m_min,m_max+1)
+    inputlist.append(f'{r} \n')
+    return r
+
+def generate_random_many(many,m_min,m_max,repeat,sort):
+    global inputlist
+    m_max += 1
+    random_lst = []
+    if repeat:
+        for i in range(int(many)):
+            random_lst.append(random.randrange(m_min,m_max))
+    else:
+        random_lst=random.sample(range(m_min,m_max),many)
+    if sort:
+        random_lst.sort()
+
+    inputlist.append(' '.join(list(map(str,random_lst)))+'\n')
+    return random_lst
+
+def generate_random_first_many(f_min,f_max,m_min,m_max,repeat,sort):
+    global inputlist
+    m_max += 1
+    f_max += 1
+    random_lst = []
+    r = random.randrange(f_min,f_max)
+    if repeat:
+        for i in range(int(r)):
+            random_lst.append(random.randrange(m_min,m_max))
+    else:
+        random_lst=random.sample(range(m_min,m_max),r)
+    if sort:
+        random_lst.sort()
+
+    inputlist.append(f'{r} '+' '.join(list(map(str,random_lst)))+'\n')
+    return random_lst
 #############################################################################
 
 # 1. 請先把解答程式放在ans.py
@@ -27,23 +66,34 @@ for i in range(secret_count):
     # 此為zj-a001的範例，直接定義secret即可
     # secret=['python','c++','hello','a123','abcdefg']
 
-    # 此為zj-a002的範例，定義兩個數的亂數
+    # 此為zj-a002的範例，自定義兩個數的亂數
     # a = random.randrange(0,10000)
     # b = random.randrange(0,10000)
     # inputlist.append(f'{a} {b}\n')
     
-    # 此為zj-d074的範例，定義亂數
-    M = random.randrange(1, 10)
-    sample_input = f"{M}"
-    inputlist.append(sample_input+"\n")
-    mlst=[]
-    for r in range(0,M):
-        m =random.randrange(10,100)
-        mlst.append(str(m))
+    # 此為zj-d074的範例，自定義亂數
+    # M = random.randrange(1, 10)
+    # sample_input = f"{M}"
+    # inputlist.append(sample_input+"\n")
+    # mlst=[]
+    # for r in range(0,M):
+    #     m =random.randrange(10,100)
+    #     mlst.append(str(m))
 
-    inputlist.append(' '.join(mlst))
+    # inputlist.append(' '.join(mlst))
+    
+
+    #此為zj-d074的呼叫方法範例
+    # generate_random_one(最小值,最大值)，會回傳一個亂數且自動塞入inputlist
+    # generate_random_many(亂數數量,最小值,最大值,是否可重複,是否排序)，會回傳一個亂數list且自動塞入inputlist
+    # generate_random_first_many(第一個數字最小值,第一個數字最大值,亂數list的最小值,亂數list的最大值,是否可重複,是否排序)
+    # 根據第一個的數字決定後面要產多少數字，會回傳一個亂數list且自動塞入inputlist
+    a = generate_random_one(1,10) # 1~10的亂數
+    generate_random_many(a,25,50,0,0)
+    print(inputlist)
+    
+    
     #測資邏輯結束
-
     #輸出secret
     if len(secret) == secret_count:
         break 
@@ -70,13 +120,14 @@ path = [
     secret_path
 ]
 
-# 建立sample跟secret資料夾
+# 建立secret資料夾
 for p in path:
     if not os.path.isdir(p):
         os.mkdir(p)
 
 # 產生input跟ans
-number = 0
+# 判斷sample中有幾筆測資
+number = len(os.listdir(os.getcwd()+'/data/sample'))//2 
 for i, d in enumerate(secret):
     number += 1
     generate_in_ans_file(d, secret_path, number)
